ea4d0e4dd1a06279a822930c650cace5ac155e2d
Strng Tn <tiansj@gmail.com>
2019-01-11 16:23:41 +0800

    Original files by blitz

---
+++ fast.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <utility>
+#include <vector>
+ 
+template <typename T>
+struct Point {
+  T x, f;
+};
+ 
+template <typename T, typename F>
+T Integrate(F f, T eps, const std::vector<T>& init_ticks) {
+  if (init_ticks.size() <= 1) {
+    return static_cast<T>(0);
+  }
+  constexpr T kHalf = static_cast<T>(0.5), kQuarter = static_cast<T>(0.25);
+  constexpr T kTwo = static_cast<T>(2), kFour = static_cast<T>(4);
+  eps = eps * kFour / (init_ticks.back() - init_ticks.front());
+  std::vector<T> areas;
+  std::vector<Point<T>> points;
+  points.reserve(init_ticks.size());
+  std::transform(init_ticks.begin(), init_ticks.end(), std::back_inserter(points),
+                 [&f](const T& x) {
+                   return Point<T>{x, f(x)};
+                 });
+  auto right = points.back();
+  for (size_t sz = points.size(); sz > 1;) {
+    auto& left = points[sz - 2];
+    T mid = (left.x + right.x) * kHalf, fmid = f(mid);
+    if (std::abs(left.f + right.f - kTwo * fmid) <= eps) {
+      areas.push_back((left.f + right.f + fmid * kTwo) * (right.x - left.x) * kQuarter);
+      points.pop_back();
+      right = left;
+      --sz;
+    } else {
+      points.back() = Point<T>{mid, fmid};
+      points.push_back(right);
+      ++sz;
+    }
+  }
+  std::sort(areas.begin(), areas.end());
+  return std::accumulate(areas.begin(), areas.end(), static_cast<T>(0));
+}
+ 
+double foo(double x) { return std::sin(x * x); }
+ 
+int main() {
+  constexpr double PI = 3.14159265358979323846;
+  for (int i = 0; i < 100; ++i) {
+    std::cout << std::setprecision(16)
+              << Integrate(foo, 1e-10, std::vector<double>{0.0, 1.0, 2.0, std::sqrt(8.0 * PI)})
+              << std::endl;
+  }
+}
